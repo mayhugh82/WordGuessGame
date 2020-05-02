5ee3bf4779c6f7317342f18bb485a38251d7b728
Diana Mayhugh <dianamayhugh@gmail.com>
2020-05-02 16:14:51 -0400

    global variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+//Global Variables
+//=====================================================
+//Arrays and Variables for holding data
+var wordOptions = ["moana", "tarzan", "cinderella", "cars","aladdin", "nemo", "ariel", "aurora", "simba"];
+var selectedWord = "";
+var lettersinWord = [];
+var numBlanks = 0;
+var blanksAndSuccesses = [];
+var wrongLetters = [];
+
+//Game counters
+var WinCount = 0;
+var lossCount = 0;
+var guessesLeft = 9;
+
+// Functions (Resusable blocks of code that I will call upon when needed)=====================================================
+function startGame () {
+    selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+    console.log(selectedWord);
+}
+
+
+startGame()
+
+//Main Process
+//=====================================================
